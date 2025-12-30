0645bb12d1c9d22adb8a145db41ce65d6f23d153
Guilherme Gazzo <guilherme@gazzo.xyz>
2025-12-30 16:50:39 -0300

    refactor(api): remove unused Meteor import from rest.ts for cleaner code

--- apps/meteor/ee/server/apps/communication/rest.ts
+++ apps/meteor/ee/server/apps/communication/rest.ts
@@ -8,7 +8,6 @@ import { License } from '@rocket.chat/license';
 import { Logger } from '@rocket.chat/logger';
 import { Settings, Users } from '@rocket.chat/models';
 import { serverFetch as fetch } from '@rocket.chat/server-fetch';
-import { Meteor } from 'meteor/meteor';
 import { ZodError } from 'zod';
 
 import { registerActionButtonsHandler } from './endpoints/actionButtonsHandler';
