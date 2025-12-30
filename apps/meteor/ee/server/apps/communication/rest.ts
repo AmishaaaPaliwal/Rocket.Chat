4f992cb6fa05991a1ac4f66e2b00b4ff80b340f5
Douglas Gubert <douglas.gubert@gmail.com>
2025-12-30 15:56:23 -0300

    refactor(apps): use route handler user instead of Meteor.userAsync()

--- apps/meteor/ee/server/apps/communication/rest.ts
+++ apps/meteor/ee/server/apps/communication/rest.ts
@@ -355,10 +355,7 @@ export class AppsRestApi {
 						return API.v1.internalError('private_app_install_disabled');
 					}
 
-					const user = orchestrator
-						?.getConverters()
-						?.get('users')
-						?.convertToApp(await Meteor.userAsync());
+					const user = orchestrator?.getConverters()?.get('users')?.convertToApp(this.user);
 
 					const aff = await manager.add(buff, {
 						...(marketplaceInfo && { marketplaceInfo }),
@@ -860,10 +857,7 @@ export class AppsRestApi {
 						return API.v1.failure({ error: 'Cannot_Update_Exempt_App' });
 					}
 
-					const user = orchestrator
-						?.getConverters()
-						?.get('users')
-						?.convertToApp(await Meteor.userAsync());
+					const user = orchestrator?.getConverters()?.get('users')?.convertToApp(this.user);
 
 					const aff = await manager.update(buff, permissionsGranted, { user, loadApp: true });
 					const info: IAppInfo & { status?: AppStatus } = aff.getAppInfo();
@@ -899,10 +893,7 @@ export class AppsRestApi {
 						return API.v1.notFound(`No App found by the id of: ${this.urlParams.id}`);
 					}
 
-					const user = orchestrator
-						?.getConverters()
-						?.get('users')
-						.convertToApp(await Meteor.userAsync());
+					const user = orchestrator?.getConverters()?.get('users').convertToApp(this.user);
 
 					const info: IAppInfo & { status?: AppStatus } = prl.getInfo();
 					try {
