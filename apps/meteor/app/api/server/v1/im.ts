aefd03b9d4111059d46ecd108a78098dd792232a
Tasso <tasso.evangelista@rocket.chat>
2025-12-31 14:02:43 -0300

    refactor: Document `im.*`/`dm.*` endpoints

--- apps/meteor/app/api/server/v1/im.ts
+++ apps/meteor/app/api/server/v1/im.ts
@@ -4,18 +4,20 @@
 import type { IMessage, IRoom, ISubscription, IUser } from '@rocket.chat/core-typings';
 import { Subscriptions, Uploads, Messages, Rooms, Users } from '@rocket.chat/models';
 import {
-	ajv,
-	validateUnauthorizedErrorResponse,
-	validateBadRequestErrorResponse,
+	createValidatorFor,
 	isDmFileProps,
 	isDmMemberProps,
 	isDmMessagesProps,
 	isDmCreateProps,
 	isDmHistoryProps,
+	BadRequestErrorResponseSchema,
+	UnauthorizedErrorResponseSchema,
+	VoidSuccessResponseSchema,
 } from '@rocket.chat/rest-typings';
 import { Match, check } from 'meteor/check';
 import { Meteor } from 'meteor/meteor';
 import type { FindOptions } from 'mongodb';
+import * as z from 'zod';
 
 import { eraseRoom } from '../../../../server/lib/eraseRoom';
 import { openRoom } from '../../../../server/lib/openRoom';
@@ -91,56 +93,13 @@ API.v1.addRoute(
 	},
 );
 
-type DmDeleteProps =
-	| {
-			roomId: string;
-	  }
-	| {
-			username: string;
-	  };
-
-const isDmDeleteProps = ajv.compile<DmDeleteProps>({
-	oneOf: [
-		{
-			type: 'object',
-			properties: {
-				roomId: {
-					type: 'string',
-				},
-			},
-			required: ['roomId'],
-			additionalProperties: false,
-		},
-		{
-			type: 'object',
-			properties: {
-				username: {
-					type: 'string',
-				},
-			},
-			required: ['username'],
-			additionalProperties: false,
-		},
-	],
-});
-
 const dmDeleteEndpointsProps = {
 	authRequired: true,
-	body: isDmDeleteProps,
+	body: createValidatorFor(z.union([z.object({ roomId: z.string() }), z.object({ username: z.string() })])),
 	response: {
-		400: validateBadRequestErrorResponse,
-		401: validateUnauthorizedErrorResponse,
-		200: ajv.compile<void>({
-			type: 'object',
-			properties: {
-				success: {
-					type: 'boolean',
-					enum: [true],
-				},
-			},
-			required: ['success'],
-			additionalProperties: false,
-		}),
+		200: createValidatorFor(VoidSuccessResponseSchema),
+		400: createValidatorFor(BadRequestErrorResponseSchema),
+		401: createValidatorFor(UnauthorizedErrorResponseSchema),
 	},
 } as const;
 
